16ae1af20150d0d989e9e131f49c8bd12fd90cec
Rittik Kumar <rkrittik17@gmail.com>
2020-07-20 20:35:25 +0530

    Project task 7

---
+++ Projects/Task7/index.js
@@ -0,0 +1,17 @@
+var myAge=19;  //declaring and storing myAge
+var earlyYears = 2; //declaring earlyAge
+
+earlyYears *= 10.5; 
+
+var laterYears = myAge-2; //subtracting 2 years from myAge 
+
+laterYears  *= 4; //multiplying laterYears by 4
+
+console.log(earlyYears);
+console.log(laterYears);
+
+myAgeInDogYears = earlyYears + laterYears; //adding earlyYears and laterYears
+
+var myName = "Rittik Kumar".toLowerCase(); //declaring myName and storing lowercase name
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`); //displaying my name and age in dog years
