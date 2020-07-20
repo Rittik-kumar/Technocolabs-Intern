6c13058c131884c68ff086bd68069da47e2c40f6
Rittik Kumar <rkrittik17@gmail.com>
2020-07-20 21:36:42 +0530

    Project task 8

---
+++ Projects/Task8/index.js
@@ -0,0 +1,47 @@
+var userName = "";
+
+if(userName==="")
+{
+  console.log("Hello!");
+}
+else
+{ 
+  console.log(`Hello, ${userName}!`);
+}
+
+var userQuestion = "What you want to ask the Magic Eight Ball ?";
+
+console.log(`${userName} ${userQuestion}`);
+
+var randomNumber = Math.floor(Math.random() * 8);
+var eightBall = "";
+
+switch(randomNumber)
+{
+  case 0 : eightBall = "It is certain";
+  break;
+
+  case 1 : eightBall = "It is decidedly so";
+  break;
+
+  case 2 : eightBall = "Reply hazy try again";
+  break;
+
+  case 3 : eightBall = "Cannot predict now";
+  break;
+
+  case 4 : eightBall = "Do not count on it";
+  break;
+
+  case 5 : eightBall = "My sources say no";
+  break;
+
+  case 6 : eightBall = "Outlook not so good";
+  break;
+
+  case 7 : eightBall = "Signs point to yes";
+  break;
+
+}
+
+console.log(eightBall);
